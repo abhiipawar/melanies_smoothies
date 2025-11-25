4e1e98545e70ff9970d2120bcdccd2145d018e9d
abhiipawar <abhiipawar@gmail.com>
2025-11-25 10:46:08 +0530

    Update streamlit_app.py
    Connection changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col,when_matched
 
 # Write directly to the app
@@ -10,7 +9,10 @@ st.write(
   """
 )
 
-session = get_active_session()
+#session = get_active_session()
+
+cnx=st.connection("snowflake")
+session=cnx.session()
 
 my_dataframe = session.table("smoothies.public.orders").select(col('ORDER_UID'),col('INGREDIENTS'),col('NAME_ON_ORDER'),col('ORDER_FILLED'))
 editable_df = st.data_editor(my_dataframe)
